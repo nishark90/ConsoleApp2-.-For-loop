c751b91be36ad3bec19e7019b74107e9e30e86fe
Nisha R K <55803996+nishark90@users.noreply.github.com>
2025-03-05 21:20:11 +0530

    Add project files.

---
+++ ConsoleApp2 . For loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.For_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            // print 1 to 10//
+        { int i;
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
